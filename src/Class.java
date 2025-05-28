8767b45e3ac053306c7bab749045d902bbbb3548
Naila010406 <nailaamelia010406@gmail.com>
2025-05-28 09:07:24 +0700

    hai

---
+++ src/Class.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.*;
+/**
+ *
+ * @author Family
+ */
+public class Class {
+
+public class Transaksi {
+    int id_transaksi;
+    double nominal;
+    String deskripsi;
+    Date tanggal;
+    String jenis;
+    String kategori;
+    String riwayatBulanan;
+}
+public class Pengguna {
+    int id_user;
+    String nama;
+    String email;
+    String password;
+}
+public class notifikasi {
+    String maksimum;
+}
+}
